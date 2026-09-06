a9760ca9b3a0643d257535fb321fad1e00ab7b5a
vibe-coder-swaroop <swroopss486@gmail.com>
2026-09-06 17:05:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ api_hash = os.environ['API_HASH']
 session_string = os.environ['TELEGRAM_SESSION']
 
 # Target channel ID
-channel_id = -1003708183148
+channel_id = 'aditya_ranjan_maths_vidyagramm'
 
 # Folder inside the runner to hold files temporarily before Rclone pushes them
-save_path = './downloads/Telegram_Archive/GK-GS/parmar_ssc/'
+save_path = './downloads/Telegram_Archive/maths/vidya gram/'
 os.makedirs(save_path, exist_ok=True)
 
 # Google Drive path checked via Rclone (Must match your .yml path)
